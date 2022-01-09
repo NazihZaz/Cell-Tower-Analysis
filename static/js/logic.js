3636de621bb838f7a0dcb36569bd4422ba3721f7
NazihZaz <nazihbouanani@gmail.com>
2022-01-09 16:03:05 -0500

    Commit of 1/9/2022

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,58 +1,48 @@
-console.log("html_results")
-function createFeatures(earthquakeData) {
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
+// Create layers for the base map
+let street= L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+})
 
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
+// Create a basemap object
+let baseMaps={
+"Street Map": street,
+"Topographic Map": top
+};
+
+// Create an overlaymaps object
+let towers=[]
+
+let overlayMaps={
+"Towers": towers
+};
+
+// Creating our initial map object:
+// We set the longitude, latitude, and starting zoom level.
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [street,towers]
+});
+
+// Add layers to our map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false,
+  options: {
+    position: 'topright'
 }
+}).addTo(myMap)
 
-function createMap(earthquakes) {
 
-  // Create the base layers.
-  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-
-  var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-
-  // Create a baseMaps object.
-  var baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
-  };
-
-  // Create an overlay object to hold our overlay.
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, earthquakes]
-  });
-
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
+// Creating a new marker:
+// We pass in some initial options, and then add the marker to the map by using the addTo() method
+for (var i = 0; i < html_results.length; i++) {
+  console.log(i.lat)
+  L.marker([i.lat,i.lon])
+    .bindPopup(`<h1>${i.cell}</h3>`)
+    .addTo(myMap);
 }
