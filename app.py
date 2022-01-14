d8f9978b9fc71cde916ad04f6aa38e6e8809ba8d
NazihZaz <nazihbouanani@gmail.com>
2022-01-13 20:31:26 -0500

    3rd commit of 1/13/2022

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def data():
 def viz():
    
     # write a statement that finds all the items in the db and sets it to a variable
-    results = list(collection.find({},{"_id":0}).limit(10000))
+    results = list(collection.find({},{"_id":0}).limit(1000))
     # return the render_template in index.hml file
     return render_template("index.html",html_results=results)
 
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,30 +1,30 @@
 // Intialize an array of radio types
 let radio = ['CDMA', 'GSM', 'LTE', 'UMTS']
 
-// Initialize the count for each type of radio
-let countCDMA = 0
-let countGSM = 0
-let countLTE = 0
-let countUMTS = 0
+// // Initialize the count for each type of radio
+// let countCDMA = 0
+// let countGSM = 0
+// let countLTE = 0
+// let countUMTS = 0
 
-// Loop through our data
-for (let i = 0; i < html_results.length; i++) {
+// // Loop through our data
+// for (let i = 0; i < html_results.length; i++) {
 
-  row = html_results[i];
+//   row = html_results[i];
 
-  if (row.radio == "CDMA") {
-    countCDMA += 1
-  }
-  else if (row.radio == "GSM") {
-    countGSM += 1
-  }
-  else if (row.radio == "LTE") {
-    countLTE += 1
-  }
-  else {
-    countUMTS += 1
-  }
-}
+//   if (row.radio == "CDMA") {
+//     countCDMA += 1
+//   }
+//   else if (row.radio == "GSM") {
+//     countGSM += 1
+//   }
+//   else if (row.radio == "LTE") {
+//     countLTE += 1
+//   }
+//   else {
+//     countUMTS += 1
+//   }
+// }
 
 // Create the data for our chart plot
 const data = {
@@ -33,7 +33,7 @@ const data = {
     label: "Count by Radio Type",
     backgroundColor: ["yellow", "blue", "red", "green"],
     borderColor: "black",
-    data: [countCDMA, countGSM, countLTE, countUMTS],
+    data: [541113, 273385,3663341,1595872],
   }]
 };
 
